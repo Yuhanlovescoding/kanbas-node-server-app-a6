55f1aa13b7d4a6282bf414b4efba23e31d48eb0c
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-11-22 19:50:01 -0800

    signin

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+
 const app = express()
 Hello(app);
 app.use(cors()); 
 app.use(express.json());
 Lab5(app);
+UserRoutes(app);
 app.listen(4000)
