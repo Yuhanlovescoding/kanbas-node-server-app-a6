d77faaa171b02c7bf650e9789f22c3ea7a3898ef
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-11-23 20:20:00 -0800

    account_screen

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -5,15 +5,48 @@ export default function UserRoutes(app) {
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
-  const updateUser = (req, res) => { };
-  const signup = (req, res) => { };
+  
+  const updateUser = (req, res) => {
+    const userId = req.params.userId;
+    const userUpdates = req.body;
+    dao.updateUser(userId, userUpdates);
+    currentUser = dao.findUserById(userId);
+    res.json(currentUser);
+   };
+  
+   const signup = (req, res) => {
+    const user = dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already in use" });
+      return;
+    }
+    currentUser = dao.createUser(req.body);
+    res.json(currentUser);
+   };
+  
   const signin = (req, res) => {
     const { username, password } = req.body;
     currentUser = dao.findUserByCredentials(username, password);
     res.json(currentUser);
    };
-  const signout = (req, res) => { };
-  const profile = (req, res) => { };
+   
+   const profile = (req, res) => {
+    if (currentUser) {
+        res.json(currentUser);
+    } else {
+        res.status(404).json({ message: "No user logged in" });
+    }
+    };
+
+
+   const signout = (req, res) => {
+    currentUser = null;
+    res.sendStatus(200);
+  };
+
+  
+
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
@@ -22,5 +55,5 @@ export default function UserRoutes(app) {
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
-  app.post("/api/users/profile", profile);
+  app.get("/api/users/profile", profile);
 }
