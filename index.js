98459a1f34cf4463d77601af03bc9d167a0bb3f6
Yuhanlovescoding <yuhanlin224@gmail.com>
2024-11-26 23:57:43 -0800

    cros

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-import express from 'express';
-import Hello from "./Hello.js"
+import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from './Kanbas/Assignments/routes.js';
-import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import EnrollmentsRoutes from "./Kanbas/Enrollments/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
-const app = express()
+const app = express();
+
+
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "https://a5--breadcrumbskanbas-react-web-app-cs561.netlify.app", 
+];
+
+
 app.use(
-    cors({
-        credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    })
-); 
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        
+        callback(null, true);
+      } else {
+        
+        callback(null, false); 
+      }
+    },
+  })
+);
+
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+  },
 };
+
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    ...sessionOptions.cookie,
+    sameSite: "none",
+    secure: true,
+  };
 }
+
 app.use(session(sessionOptions));
-   
 app.use(express.json());
+
 
 Hello(app);
 Lab5(app);
@@ -41,6 +63,8 @@ CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentsRoutes(app);
+
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
